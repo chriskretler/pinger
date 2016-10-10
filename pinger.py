9741fd146b48a7193cb87c7dde3619c1bff7604b
Chris <ckretler@umich.edu>
2016-10-10 18:38:22 -0400

    make verbose output for host if response time is too long.

--- pinger.py
+++ pinger.py
@@ -12,16 +12,21 @@ def api_handler(i):
 
    try:
       begin = datetime.now()
-#	  call(["host", "google.com"])
+      #call(["host", "google.com", ">", "response"])
       headers = {CONTENT_TYPE: MIME_TYPE_JSON}
-      response = requests.get("https://www.google.com/logos/doodles/2016/2016-doodle-fruit-games-day-8-5666133911797760.3-hp.gif", headers=headers)
-	  #dig
-               
+      #response = requests.get("https://www.google.com/logos/doodles/2016/2016-doodle-fruit-games-day-8-5666133911797760.3-hp.gif", headers=headers)
+      response = requests.get("https://www.google.com", headers=headers)
+
       end = datetime.now()
       diff = end - begin
-      print str(i) + " execution time: " + str(diff)             
+      print str(i) + " execution time: " + str(diff.total_seconds())
+
+      if diff.total_seconds() > 3:
+        print "clock time: " + str(end)            
+        print call(["host", "-v", "google.com"])
                 
-   except (requests.exceptions.RequestException, Exception) as exception:
+#   except (requests.exceptions.RequestException, Exception) as exception:
+   except (Exception) as exception:
       raise exception
 
    return response
@@ -29,8 +34,8 @@ def api_handler(i):
 
 print 'started at: ' + str(datetime.now())
 
-for i in range(1, 100):
+for i in range(1, 10000):
    api_handler(i)
-   time.sleep(1)
+   time.sleep(3)
 
 print 'ended at: ' + str(datetime.now())
