2ef708548bc44efa8ca5b7db3fde1eb4e5c87192
chriskretler <ckretler@umich.edu>
2019-03-21 17:11:18 -0400

    less verbose logging.

--- pinger.py
+++ pinger.py
@@ -12,14 +12,15 @@ def api_handler():
 
    try:
       begin = datetime.now()
-      call(["host", "-v", "dig-mysql.miserver.it.umich.edu"])
+      #call(["host", "-v", "dig-mysql.miserver.it.umich.edu"])
+      call(["host", "dig-mysql.miserver.it.umich.edu"])
       #headers = {CONTENT_TYPE: MIME_TYPE_JSON}
       #response = requests.get("https://www.google.com", headers=headers)
 
       end = datetime.now()
       diff = end - begin
       #print str(i) + " execution time: " + str(diff.total_seconds())
-      print "execution time: " + str(diff.total_seconds())
+      print "execution time: " + str(diff.total_seconds()) + "\n"
 
       #if diff.total_seconds() > 3:
       #  print "clock time: " + str(end)            
