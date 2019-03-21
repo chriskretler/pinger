6bd7d89eaafe6eef9459b7b9b989bcc9db3ddfe9
chriskretler <ckretler@umich.edu>
2019-03-21 17:04:13 -0400

    modified script to look at miserver db.

--- pinger.py
+++ pinger.py
@@ -6,24 +6,24 @@ from subprocess import call
 MIME_TYPE_JSON = 'application/json'
 CONTENT_TYPE = 'Content-type'
 
-def api_handler(i):
+def api_handler():
 
    response = None
 
    try:
       begin = datetime.now()
-      #call(["host", "google.com", ">", "response"])
-      headers = {CONTENT_TYPE: MIME_TYPE_JSON}
-      #response = requests.get("https://www.google.com/logos/doodles/2016/2016-doodle-fruit-games-day-8-5666133911797760.3-hp.gif", headers=headers)
-      response = requests.get("https://www.google.com", headers=headers)
+      call(["host", "-v", "dig-mysql.miserver.it.umich.edu"])
+      #headers = {CONTENT_TYPE: MIME_TYPE_JSON}
+      #response = requests.get("https://www.google.com", headers=headers)
 
       end = datetime.now()
       diff = end - begin
-      print str(i) + " execution time: " + str(diff.total_seconds())
+      #print str(i) + " execution time: " + str(diff.total_seconds())
+      print "execution time: " + str(diff.total_seconds())
 
-      if diff.total_seconds() > 3:
-        print "clock time: " + str(end)            
-        print call(["host", "-v", "google.com"])
+      #if diff.total_seconds() > 3:
+      #  print "clock time: " + str(end)            
+      #  print call(["host", "-v", "google.com"])
                 
 #   except (requests.exceptions.RequestException, Exception) as exception:
    except (Exception) as exception:
@@ -34,8 +34,9 @@ def api_handler(i):
 
 print 'started at: ' + str(datetime.now())
 
-for i in range(1, 100000):
-   api_handler(i)
+#for i in range(1, 100000):
+while(True):
+   api_handler()
    time.sleep(3)
 
 print 'ended at: ' + str(datetime.now())
